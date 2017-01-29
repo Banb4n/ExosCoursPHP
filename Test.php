973e4dfce0e4d8874d80a970ec907aee84f34009
Alban <alban.such@gmail.com>
2017-01-29 17:51:12 +0100

    Exercice OpenClassroom
    Added exercices of OPC

---
+++ Test.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+ <title>Premier utilisation OPC</title>
+</head>
+<body>
+  <h1>Page de test</h1>
+
+  <p>
+    Cette page contient du code Html ainsi que du code PHP.<br />
+    <?php echo "Bonjour !"; ?>
+    Voici quelques petits tests: 
+  </p>
+  
+  <ul>
+    <li style="color: blue;">Je suis bleu</li>
+    <li style="color: red;">Je suis rouge</li>
+    <li style="color: green;">Je suis vert et alors</li>
+  </ul>
+</body>
+</html>
